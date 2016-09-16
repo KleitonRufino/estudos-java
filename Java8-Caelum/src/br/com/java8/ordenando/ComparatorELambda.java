2418c2a15a63e3645b3e9c497eba3654309c98aa
Kleiton Rufino <kleiton.arufino@gmail.com>
2016-09-16 11:01:34 -0300

    inserindo novo metodo

--- Java8-Caelum/src/br/com/java8/ordenando/ComparatorELambda.java
+++ Java8-Caelum/src/br/com/java8/ordenando/ComparatorELambda.java
@@ -24,6 +24,31 @@ public class ComparatorELambda {
 
 	}
 
+
+	public void comparatorNull(){
+		Comparator<Usuario> c1 = Comparator.comparing(Usuario::getNome,
+				Comparator.nullsLast(Comparator.naturalOrder()));
+		
+		Comparator<Usuario> c2 = Comparator.comparing(u1 -> u1.getNome(),
+				Comparator.nullsLast(Comparator.naturalOrder()));
+		
+		
+		Comparator<Usuario> c3 = (u1, u2) -> String.CASE_INSENSITIVE_ORDER.compare(Optional.ofNullable(u1.getNome()).toString(), Optional.ofNullable(u2.getNome()).toString());
+		
+		Comparator<Usuario> c4 = Comparator.comparing(Usuario::getPontos,
+				Comparator.nullsLast(Comparator.naturalOrder()));
+		
+		Comparator<Usuario> c5 = Comparator.comparing(Usuario::getPontos,
+				Comparator.nullsLast(Comparator.naturalOrder())).reversed();
+		
+		
+		Collections.sort(usuarios, c3);
+		
+		usuarios.forEach(u -> {
+			System.out.println(u.getNome());
+		});
+	}
+	
 	@SuppressWarnings("unused")
 	public void ornedaComparatorModoI() {
 		Comparator<Usuario> comparator = (u1, u2) -> u1.getNome().compareTo(
